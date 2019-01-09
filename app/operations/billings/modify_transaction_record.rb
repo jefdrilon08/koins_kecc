11f41b65b1ad85e6646587d92fdff89d0f8f9280
Raphael Alampay <raphael.alampay@gmail.com>
2019-01-09 10:34:24 +0800

    checkpoint

--- app/operations/billings/modify_transaction_record.rb
+++ app/operations/billings/modify_transaction_record.rb
@@ -125,6 +125,7 @@ module Billings
             r[:records].each_with_index do |rr, j|
               if rr[:record_type] == "WP"
                 #total_collected += rr[:amount].try(:to_f).round(3)
+                total_collected -= rr[:amount].try(:to_f).round(2)
                 @data[:totals][index][:amount] += rr[:amount].try(:to_f).round(2)
               end
             end
