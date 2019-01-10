7536632da74dd48d5398e1fc905593ce79ea906a
Raphael Alampay <raphael.alampay@gmail.com>
2019-01-10 15:46:14 +0800

    checkpoint

--- app/operations/monitoring/accounting_entry_subsidiary_balancing.rb
+++ app/operations/monitoring/accounting_entry_subsidiary_balancing.rb
@@ -18,6 +18,11 @@ module Monitoring
       end
 
       @settings_insurance_accounting_codes  = Settings.insurance_accounting_codes
+
+      if @settings_insurance_accounting_codes.blank?
+        raise "settings_insurance_accounting_codes not found"
+      end
+
       @settings_equity_accounting_codes     = Settings.equity_accounting_codes
 
       @journal_entries  = JournalEntry.joins(:accounting_entry).where(
@@ -37,7 +42,7 @@ module Monitoring
 
     def execute!
       fetch_loans_receivables!
-      #fetch_personal_funds!
+      fetch_personal_funds!
 
       @data
     end
@@ -83,13 +88,58 @@ module Monitoring
 
         account_transactions  = AccountTransaction.savings.where(subsidiary_id: member_accounts.pluck(:id)).where("DATE(transacted_at) <= ?", @as_of).order("transacted_at ASC")
 
-        member_accounts.each do |a|
-          t = AccountTransaction.savings.where(subsidiary_id: a.id).where("DATE(transacted_at) <= ?", @as_of).order("transacted_at ASC, created_at ASC").last
+        deposits    = account_transactions.where(transaction_type: "deposit").sum(:amount)
+        withdrawals = account_transactions.where(transaction_type: "withdraw").sum(:amount)
 
-          if t.present?
-            d[:subsidiary_balance] += t.data.with_indifferent_access[:ending_balance].try(:to_f).round(2)
-          end
-        end
+        d[:subsidiary_balance]  = deposits - withdrawals
+
+        d[:diff]  = (d[:accounting_entry_balance] - d[:subsidiary_balance]).round(2)
+
+        @data[:personal_funds] << d 
+      end
+
+      # EQUITY
+      @settings_equity_accounting_codes.each do |o|
+        account_type    = "EQUITY"
+        account_subtype = o.equity_type
+
+        d = {
+          accounting_code: {
+            id: "",
+            name: "",
+            code: ""
+          },
+          account: {
+            account_type: account_type,
+            account_subtype: account_subtype
+          },
+          accounting_entry_balance: 0.00,
+          subsidiary_balance: 0.00,
+          diff: 0.00
+        }
+
+        accounting_code = AccountingCode.find(o.deposit_accounting_code_id)
+        d[:accounting_code][:id]    = accounting_code.id
+        d[:accounting_code][:name]  = accounting_code.name
+        d[:accounting_code][:code]   = accounting_code.code
+
+        debit_amount  = @journal_entries.where(accounting_code_id: accounting_code.id, post_type: 'DR').sum(:amount)
+        credit_amount = @journal_entries.where(accounting_code_id: accounting_code.id, post_type: 'CR').sum(:amount)
+
+        d[:accounting_entry_balance]  = (credit_amount - debit_amount).round(2)
+
+        member_accounts = MemberAccount.where(
+                            account_type: account_type,
+                            account_subtype: account_subtype,
+                            branch_id: @branch.id
+                          )
+
+        account_transactions  = AccountTransaction.savings.where(subsidiary_id: member_accounts.pluck(:id)).where("DATE(transacted_at) <= ?", @as_of).order("transacted_at ASC")
+
+        deposits    = account_transactions.where(transaction_type: "deposit").sum(:amount)
+        withdrawals = account_transactions.where(transaction_type: "withdraw").sum(:amount)
+
+        d[:subsidiary_balance]  = deposits - withdrawals
 
         d[:diff]  = (d[:accounting_entry_balance] - d[:subsidiary_balance]).round(2)
 
