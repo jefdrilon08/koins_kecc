b95c2fd38b25654cb1b402455193eb1f4269c7b6
Raphael Alampay <raphael.alampay@gmail.com>
2019-01-17 15:01:46 +0800

    year end closing validation for create

--- app/controllers/api/v1/data_stores/year_end_closings_controller.rb
+++ app/controllers/api/v1/data_stores/year_end_closings_controller.rb
@@ -8,7 +8,16 @@ module Api
           @data_store_type  = "YEAR_END_CLOSING"
           @record           = DataStore.year_end_closings.where(id: params[:id]).first 
 
-          if @record.blank?
+          @errors = ::Closing::ValidateYearEndGenerate.new(
+                      config: {
+                        branch: Branch.where(id: params[:branch_id]).first,
+                        closing_date: params[:closing_date].try(:to_date)
+                      }
+                    ).execute!
+
+          if @errors[:full_messages].size > 0
+            render json: @errors, status: 400
+          elsif @record.blank?
             @branch       = Branch.find(params[:branch_id])
             @closing_date = params[:closing_date].to_date
 
@@ -25,17 +34,17 @@ module Api
                           status: "processing"
                         }
                       )
+
+            args = {
+              id: @record.id,
+              data_store_type: @data_store_type,
+              user_id: current_user.id
+            }
+
+            ProcessYearEndClosing.perform_later(args)
+
+            render json: { message: "ok" }
           end
-
-          args = {
-            id: @record.id,
-            data_store_type: @data_store_type,
-            user_id: current_user.id
-          }
-
-          ProcessYearEndClosing.perform_later(args)
-
-          render json: { message: "ok" }
         end
       end
     end
---
+++ app/operations/closing/validate_year_end_generate.rb
@@ -0,0 +1,46 @@
+module Closing
+  class ValidateYearEndGenerate < AppValidator
+    def initialize(config:)
+      super()
+  
+      @config       = config
+      @branch       = @config[:branch]
+      @closing_date = @config[:closing_date].try(:to_date)
+    end
+
+    def execute!
+      if @branch.blank?
+        @errors[:messages] << {
+          key: "branch",
+          message: "Branch not found"
+        }
+      end
+
+      if @closing_date.blank?
+        @errors[:messages] << {
+          key: "closing_date",
+          message: "Closing date not found"
+        }
+      end
+
+      if @branch.present? and @closing_date.present?
+        year  = @closing_date.year
+
+        if DataStore.year_end_closings.where("CAST(meta->>'year' AS integer) = ?", year).count > 0
+          @errors[:messages] << {
+            key: "closing_date",
+            message: "Already has closing record for year #{@year}"
+          }
+        end
+      end
+
+      #not_yet_implemented!
+
+      @errors[:messages].each do |o|
+        @errors[:full_messages] << o[:message]
+      end
+
+      @errors
+    end
+  end
+end
