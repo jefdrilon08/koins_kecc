28db795fb667348d27c0bccb81188455a200b97e
Raphael Alampay <raphael.alampay@gmail.com>
2019-02-01 16:29:29 +0800

    bug fix for depositing funds on loan approval

--- app/operations/loans/approve.rb
+++ app/operations/loans/approve.rb
@@ -120,7 +120,7 @@ module Loans
 
               amount  = val * (multiplier + offset)
             else
-              amount  = s_deduction.amount
+              amount  = val
             end
 
             #### DEPOSIT TRANSACTION ####
