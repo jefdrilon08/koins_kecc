a6b17331d89a5418c1d4849bb969e91c657a272e
Raphael Alampay <raphael.alampay@gmail.com>
2019-03-12 10:28:14 +0800

    show members without certificates

--- app/controllers/administration/member_shares_controller.rb
+++ app/controllers/administration/member_shares_controller.rb
@@ -4,12 +4,14 @@ module Administration
 
     def index
       @member_shares  = MemberShare.not_printed.joins(:member).where("members.branch_id IN (?)", @branches.pluck(:id))
+      @members        = Member.active.where.not(id: MemberShare.all.pluck(:member_id).uniq).where(branch_id: @branches.pluck(:id)).order("last_name ASC")
 
       if params[:branch_id].present?
         @branch_id  = params[:branch_id]
         @branch     = Branch.find(@branch_id)
 
         @member_shares  = @member_shares.where("members.branch_id = ?", @branch.id)
+        @members        = @members.where(branch_id: @branch.id)
       end
 
       @member_shares  = @member_shares.page(params[:page]).per(20)
