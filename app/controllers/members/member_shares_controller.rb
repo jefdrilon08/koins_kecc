6bd92cfb46aeaf019f6720982520669c4505479d
Raphael Alampay <raphael.alampay@gmail.com>
2019-03-14 08:49:00 +0800

    checkpoint

--- app/controllers/members/member_shares_controller.rb
+++ app/controllers/members/member_shares_controller.rb
@@ -39,7 +39,7 @@ module Members
 
     def flag_as_printed
       @member_share = MemberShare.find(params[:member_share_id])
-      @member_share.update!(data: { is_printed: true, date_printed: Date.today})
+      @member_share.update!(data: { printed: true, date_printed: Date.today})
 
       redirect_to member_member_share_path(@member_share.member.id, @member_share.id)
     end
@@ -51,7 +51,7 @@ module Members
       if @member_share.update(member_share_params)
         data  = @member_share.data.with_indifferent_access
 
-        data[:is_printed] = false
+        data[:printed] = false
 
         @member_share.update!(data: data)
 
