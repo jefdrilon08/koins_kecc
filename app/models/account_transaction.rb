1583debeae9ef417b4855185fab7ad40927e8778
Raphael Alampay <raphael.alampay@gmail.com>
2019-06-30 12:30:44 +0800

    Fix in member counts for pure savers

--- app/models/account_transaction.rb
+++ app/models/account_transaction.rb
@@ -4,9 +4,9 @@ class AccountTransaction < ApplicationRecord
   scope :personal_funds, -> { where(transaction_type: ["deposit", "withdraw"]) }
   scope :personal_funds_deposits, -> { where(transaction_type: "deposit") }
   scope :personal_funds_withdrawals, -> { where(transaction_type: "withdraw") }
-  scope :savings, -> { where(transaction_type: ["deposit", "withdraw"]).order("transacted_at ASC") }
-  scope :savings_deposits, -> { where(transaction_type: "deposit").order("transacted_at ASC") }
-  scope :savings_withdrawals, -> { where(transaction_type: "withdraw").order("transacted_at ASC") }
+  scope :savings, -> { where(transaction_type: ["deposit", "withdraw"]).order("transacted_at ASC, updated_at ASC") }
+  scope :savings_deposits, -> { where(transaction_type: "deposit").order("transacted_at ASC, updated_at ASC") }
+  scope :savings_withdrawals, -> { where(transaction_type: "withdraw").order("transacted_at ASC, updated_at ASC") }
   scope :approved_loan_payments, -> { where("transaction_type = ? AND amount > 0", "loan_payment").order("transacted_at ASC") }
   scope :approved, -> { where(status: "approved") }
   scope :interest, -> { where("transaction_type = ? AND CAST(data->>'is_interest' AS boolean) = ?", "deposit", 't') }
--- app/operations/stats/compute_pure_savers.rb
+++ app/operations/stats/compute_pure_savers.rb
@@ -24,67 +24,46 @@ module Stats
 
     def execute!
       @members.each do |m|
-        # Check if we have paid loans
-        active_loans  = ::Loans::FetchActiveAsOf.new(
+        total_balance = ::Members::FetchTotalBalance.new(
                           config: {
                             member: m,
                             as_of: @as_of
                           }
                         ).execute!
 
-        if active_loans.size == 0
-          member_accounts = MemberAccount.savings.where(
-                              "members.id = ?", m.id
-                            )
-
-          total_balance = 0.00
-
-          member_accounts.each do |a|
-            latest_transaction  = AccountTransaction.savings.where(
-                                    "DATE(transacted_at) <= ? AND subsidiary_id = ?",
-                                    @as_of,
-                                    a.id
-                                  ).last
-
-            if latest_transaction.present?
-              total_balance += latest_transaction.data["ending_balance"].to_f.round(2)
-            end
+        if total_balance > 0
+          if m.gender == "Male"
+            @data[:male] += 1
+          elsif m.gender == "Female"
+            @data[:female] += 1
+          else
+            @data[:others] += 1
           end
 
-          if total_balance > 0
-            if m.gender == "Male"
-              @data[:male] += 1
-            elsif m.gender == "Female"
-              @data[:female] += 1
-            else
-              @data[:others] += 1
-            end
+          @data[:total] += 1
 
-            @data[:total] += 1
-
-            @data[:members] << {
-              id: m.id,
-              identification_number: m.identification_number,
-              first_name: m.first_name,
-              middle_name: m.middle_name,
-              last_name: m.last_name,
-              member_type: m.member_type,
-              total_balance: total_balance,
-              branch: {
-                id: m.branch.id,
-                name: m.branch.name
-              },
-              center: {
-                id: m.center.id,
-                name: m.center.name
-              },
-              officer: {
-                id: m.center.user.id,
-                first_name: m.center.user.first_name,
-                last_name: m.center.user.last_name
-              }
+          @data[:members] << {
+            id: m.id,
+            identification_number: m.identification_number,
+            first_name: m.first_name,
+            middle_name: m.middle_name,
+            last_name: m.last_name,
+            member_type: m.member_type,
+            total_balance: total_balance,
+            branch: {
+              id: m.branch.id,
+              name: m.branch.name
+            },
+            center: {
+              id: m.center.id,
+              name: m.center.name
+            },
+            officer: {
+              id: m.center.user.id,
+              first_name: m.center.user.first_name,
+              last_name: m.center.user.last_name
             }
-          end
+          }
         end
       end
 
