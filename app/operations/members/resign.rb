13d27b7e478a33cfbb337f42f0811c281ecbd698
hannahgraceb <bulataohannahgrace@gmail.com>
2019-07-12 16:18:31 +0800

    add daily report insurance account generate

---
+++ app/operations/members/resign.rb
@@ -0,0 +1,24 @@
+module Members
+  class Resign
+    def initialize(member:, date_resigned:, resigned_by:, reason:)
+      @member         = member
+      @date_resigned  = date_resigned
+      @resigned_by    = resigned_by
+      @member_data    = @member.data.with_indifferent_access
+      @reason         = reason
+    end
+
+    def execute!
+      @member_data[:resignation][:reason] = @reason
+      @member.update!(data: @member_data)
+
+      @member.update!(
+        status: 'resigned', 
+        date_resigned: @date_resigned,
+        insurance_status: 'resigned',
+        date_resigned: @date_resigned,
+        insurance_date_resigned: @date_resigned
+        )
+    end
+  end
+end
