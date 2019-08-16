4b8d3775a90a80bb252df3c1ea0dba88be15b68a
Aljon Laureano <laureanoaljon0392@gmail.com>
2019-08-16 17:36:12 +0800

    hotfix

--- app/operations/insurance/import_insurance_account_transactions_from_csv_file.rb
+++ app/operations/insurance/import_insurance_account_transactions_from_csv_file.rb
@@ -13,14 +13,14 @@ module Insurance
     def load_csv_file!
       insurance_account_ids = []
 
-      CSV.foreach(@file, headers: true) do |row|
+      CSV.foreach(@file.path, headers: true) do |row|
         uuid = row['uuid']
-        insurance_account_transaction_record = AccountTransaction.where(uuid: uuid).first
+        insurance_account_transaction_record = AccountTransaction.where(id: uuid).first
 
         if insurance_account_transaction_record.nil?
           insurance_account_transaction = AccountTransaction.new
-          
-          amount = row['amount']
+
+          insurance_account_transaction.id = uuid
           insurance_account_transaction.transacted_at = row['transacted_at']
           insurance_account_transaction.status = row['status']
           
@@ -39,41 +39,48 @@ module Insurance
                                                 }
         
           statuses = ["active", "inactive"]
-          insurance_account = MerberAccount.where("uuid = ? AND status IN (?)", insurance_account_uuid, statuses).first
+          insurance_account = MemberAccount.where("id = ? AND status IN (?)", row['insurance_account_uuid'], statuses).first
           # insurance_account = InsuranceAccount.where("uuid = ? AND status = ?", insurance_account_uuid, "active").first
 
-          insurance_account_transaction.insurance_account_id = insurance_account.id
-          transaction_type = row['transaction_type']
-          insurance_account_transaction.amount = amount
-          insurance_account_transaction.transaction_type = transaction_type
+          insurance_account_transaction.subsidiary_id = insurance_account.id
+          insurance_account_transaction.subsidiary_type = "MemberAccount"
+          insurance_account_transaction.transaction_type = row['transaction_type']
+          insurance_account_transaction.amount = row['amount']
           
           insurance_account_transaction.save!
           
-          insurance_account_ids << insurance_account_transaction.insurance_account.id
+          insurance_account_ids << insurance_account_transaction.subsidiary_id
         else
+          insurance_account_transaction_record_data = insurance_account_transaction_record.data.with_indifferent_access
+
+          insurance_account_transaction_record_data[:is_withdraw_payment] = false
+          insurance_account_transaction_record_data[:is_fund_transfer] = false
+          insurance_account_transaction_record_data[:is_interest] = false
+          insurance_account_transaction_record_data[:is_adjustment] = row['is_adjustment']
+          insurance_account_transaction_record_data[:is_for_exit_age] = row['for_exit_age']
+          insurance_account_transaction_record_data[:is_for_loan_payments] = row['for_loan_payments']
+          insurance_account_transaction_record_data[:accounting_entry_reference_number] = row['voucher_reference_number']
+          insurance_account_transaction_record_data[:accounting_entry_particular] = row['particular']
+          insurance_account_transaction_record_data[:beginning_balance] = row['beginning_balance']
+          insurance_account_transaction_record_data[:ending_balance] = row['ending_balance']
+
           insurance_account_transaction_record.update!(
             amount: row['amount'],
+            subsidiary_id: row['insurance_account_uuid'],
             transaction_type: row['transaction_type'],
             transacted_at: row['transacted_at'],
-            particular: row['particular'],
             status: row['status'],
-            transacted_by: row['transacted_by'],
-            approved_by: row['approved_by'],
-            voucher_reference_number: row['voucher_reference_number'],
-            transaction_number: row['transaction_number'],
-            uuid: row['uuid'],
-            transaction_date: row['transaction_date'],
-            is_adjustment: row['is_adjustment']
+            data: insurance_account_transaction_record_data
           )
 
-          insurance_account_ids << insurance_account_transaction_record.insurance_account.id
+          insurance_account_ids << insurance_account_transaction_record.subsidiary_id
         end
       end
 
       insurance_account_ids = insurance_account_ids.uniq
 
-      InsuranceAccount.where(id: insurance_account_ids).each do |acc|
-        ::Insurance::RehashAccount.new(insurance_account: acc).execute!
+      MemberAccount.where(id: insurance_account_ids).each do |acc|
+        ::MemberAccounts::Rehash.new(member_account: acc).execute!
       end
     end
   end
