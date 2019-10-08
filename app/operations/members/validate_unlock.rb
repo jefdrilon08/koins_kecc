7a30a6c76b5a588cdfe78b811d64389e76b0bfc1
Aljon Laureano <laureanoaljon0392@gmail.com>
2019-10-08 10:04:13 +0800

    modify sidebar for remote users

--- app/operations/members/validate_unlock.rb
+++ app/operations/members/validate_unlock.rb
@@ -7,7 +7,7 @@ module Members
       @member = @config[:member]
       @user   = @config[:user]
 
-      @valid_roles = ["MIS", "BK", "SBK", "REMOTE-FM"]
+      @valid_roles = ["MIS", "BK", "SBK", "REMOTE-BK", "REMOTE-FM"]
     end
 
     def execute!
