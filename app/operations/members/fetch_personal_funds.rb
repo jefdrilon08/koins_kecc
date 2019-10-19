96398d459e7ae700c50dffd8144a5ed2aee79a51
Raphael Alampay <raphael.alampay@gmail.com>
2019-10-19 11:38:14 +0800

    additional fixes for personal funds computation

--- app/operations/members/fetch_personal_funds.rb
+++ app/operations/members/fetch_personal_funds.rb
@@ -23,14 +23,20 @@ module Members
     def execute!
       member_accounts = MemberAccount.joins(
                           "INNER JOIN account_transactions ON member_accounts.id = account_transactions.subsidiary_id"
+                        ).joins(
+                          "INNER JOIN members ON member_accounts.member_id = members.id"
+                        ).joins(
+                          "INNER JOIN branches ON members.branch_id = branches.id"
+                        ).joins(
+                          "INNER JOIN centers ON members.center_id = centers.id"
                         ).where(
                           "account_transactions.transacted_at <= ? AND member_accounts.member_id = ?", 
                           @as_of,
                           @member.id
                         ).select(
-                          "DISTINCT ON(member_accounts.id, account_transactions.transacted_at, member_accounts.member_id) member_accounts.id, member_accounts.member_id, member_accounts.account_type, member_accounts.account_subtype, DATE(transacted_at), account_transactions.data"
+                          "DISTINCT ON(member_accounts.id, account_transactions.transacted_at, member_accounts.member_id, account_transactions.updated_at) member_accounts.id, member_accounts.member_id, member_accounts.account_type, member_accounts.account_subtype, DATE(transacted_at), account_transactions.data, branches.id AS branch_id, branches.name AS branch_name"
                         ).order(
-                          "account_transactions.transacted_at DESC"
+                          "account_transactions.transacted_at DESC, account_transactions.updated_at DESC"
                         )
 
       @default_member_accounts.each do |s|
@@ -39,13 +45,21 @@ module Members
           account_subtype: s.account_subtype,
           member_id: @member.id,
           balance: 0.00,
-          account_transaction_id: ""
+          account_transaction_id: "",
+          branch: {
+            id: "",
+            name: ""
+          }
         }
 
         m_account = member_accounts.select{ |o| o[:account_type] == s.account_type && o[:account_subtype] == s.account_subtype }.first
 
         if m_account.present?
           account[:balance] = m_account.data["ending_balance"].to_f.round(2)
+          account[:branch]  = {
+            id: m_account.branch_id,
+            name: m_account.branch_name
+          }
         end
 
         @data[:accounts] << account
