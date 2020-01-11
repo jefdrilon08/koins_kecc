5a0687709c6f77dbe0e1ed83fca8ab8635bbdfe8
Raphael Alampay <raphael.alampay@gmail.com>
2020-01-11 10:50:22 +0800

    checkpoint

--- config/schedule.rb
+++ config/schedule.rb
@@ -27,6 +27,7 @@
 
 set :environment, "production"
 set :output, {:error => "log/cron_error_log.log", :standard => "log/cron_log.log"}
+env :PATH, ENV['PATH']
 
 #every :day, at: '1am' do
 every 1.minutes do
