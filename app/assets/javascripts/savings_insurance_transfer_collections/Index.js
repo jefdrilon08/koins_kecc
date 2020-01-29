100c7a780443855eb357fc9027b84d91126ca20a
Raphael Alampay <raphael.alampay@gmail.com>
2020-01-29 16:07:46 +0800

    interface for savins insurance transfer collection modal

--- app/assets/javascripts/savings_insurance_transfer_collections/Index.js
+++ app/assets/javascripts/savings_insurance_transfer_collections/Index.js
@@ -2,14 +2,17 @@ var Index = (function() {
   var $btnNewTransaction;
   var $modalNewTransaction;
   var $selectBranch;
+  var $selectCenter;
   var $inputCollectionDate;
 
   var _options;
+  var _authenticityToken;
 
   var _cacheDom = function() {
     $btnNewTransaction    = $("#btn-new-transaction");
     $modalNewTransaction  = $("#modal-new-transaction");
     $selectBranch         = $("#select-branch");
+    $selectCenter         = $("#select-center");
     $inputCollectionDate  = $("#input-collection-date");
   };
 
@@ -20,7 +23,8 @@ var Index = (function() {
   };
 
   var init  = function(options) {
-    _options  = options;
+    _options            = options;
+    _authenticityToken  = _options.authenticityToken;
 
     _cacheDom();
     _bindEvents();
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -14,6 +14,7 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 
 Rails.application.config.assets.precompile += [
+  "savings_insurance_transfer_collections/Index.js",
   "login.scss",
   "member.scss",
   "Login.js",
