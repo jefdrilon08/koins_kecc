e1ace619897977c316be1f1e7f6ccf868990b1e8
Raphael Alampay <raphael.alampay@gmail.com>
2020-02-03 12:28:43 +0800

    initial operation for fetching inforced members

---
+++ app/operations/insurance/fetch_inforced.rb
@@ -0,0 +1,71 @@
+module Insurance
+  class FetchInforced
+    def initialize(config:)
+      @config             = config
+      @branch             = @config[:branch]
+      @as_of              = @config[:as_of].try(:to_date)
+      @insurance_subtype  = @config[:insurance_subtype]
+      @default_amount     = @config[:default_amount].to_f.round(2)
+
+      @data = {
+        as_of: @as_of,
+        insurance_type: @insurance_type,
+        default_amount: @default_amount,
+        branch: {
+          id: @branch.id,
+          name: @branch.name
+        },
+        records: []
+      }
+    end
+
+    def execute!
+      query!
+
+      @data[:records] = @result.select{ |o|
+                          insured_amount  = (((@as_of - o.fetch("recognition_date").to_date).to_i / 7).to_i + 1) * @default_amount
+                          ending_balance  = o.fetch("ending_balance").to_f.round(2)
+
+                          o[:insured_amount]  = insured_amount
+
+                          ending_balance >= insured_amount
+                        }
+
+      @data
+    end
+
+    private
+
+    def query!
+      @result = ActiveRecord::Base.connection.execute(<<-EOS).to_a
+                  SELECT DISTINCT ON(members.identification_number, member_accounts.id)
+                    members.id AS member_id,
+                    members.first_name,
+                    members.middle_name,
+                    members.last_name,
+                    members.status AS member_status,
+                    members.insurance_status AS insurance_status,
+                    members.data->'recognition_date' AS recognition_date,
+                    members.identification_number,
+                    member_accounts.id AS member_account_id,
+                    member_accounts.account_type,
+                    member_accounts.account_subtype,
+                    centers.id AS center_id,
+                    centers.name AS center_name,
+                    t1.transacted_at AS latest_transaction_date,
+                    t1.amount AS latest_transaction_amount,
+                    t1.data->>'ending_balance' AS ending_balance
+                  FROM
+                    member_accounts
+                  INNER JOIN members ON
+                    member_accounts.member_id = members.id AND member_accounts.account_type = 'INSURANCE' AND member_accounts.account_subtype = '#{@insurance_subtype}' AND members.branch_id = '#{@branch.id}' AND members.data->'recognition_date' IS NOT NULL
+                  INNER JOIN centers ON
+                    centers.id = member_accounts.center_id
+                  LEFT JOIN account_transactions AS t1 ON
+                    t1.subsidiary_id = member_accounts.id AND t1.status = 'approved' AND DATE(t1.transacted_at) < DATE('#{@as_of}')
+                  ORDER BY
+                    members.identification_number, member_accounts.id, members.last_name ASC, t1.transacted_at DESC, t1.updated_at DESC
+                EOS
+    end
+  end
+end
