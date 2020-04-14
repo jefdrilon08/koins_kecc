e4b4eef81196b6ee1ca9f609581e812257e30fdc
Mis <mis2kdci@gmail.com>
2020-04-14 12:03:22 +0800

    datastore filter

--- react/data_stores/x_weeks_to_pay/Show.js
+++ react/data_stores/x_weeks_to_pay/Show.js
@@ -7,11 +7,18 @@ import ShowComponent from "./ShowComponent";
 var $parameters       = $("#parameters");
 var authenticityToken = $("meta[name='csrf-token']").attr('content');
 var id                = $parameters.data("id");
+var start_date        = $parameters.data("start_date");
+var end_date          = $parameters.data("end_date");
+var branch_id         = $parameters.data("branch_id");
+
 
 ReactDOM.render(
   <ShowComponent
     authenticityToken={authenticityToken}
     id={id}
+    start_date={start_date}
+    end_date={end_date}
+    branch_id={branch_id}
   />,
   document.getElementById('content')
 );
