4f2fdfd09f057548abc73ee8697096ebed6fc7fe
Raphael Alampay <raphael.alampay@gmail.com>
2020-06-17 08:13:56 +0800

    remove inheritted scopes

--- app/models/read_only_loan.rb
+++ app/models/read_only_loan.rb
@@ -1,9 +1,3 @@
-class ReadOnlyLoan < Branch
+class ReadOnlyLoan < Loan
   establish_connection(ENV['FOLLOWER_READ_ONLY_DATABASE_URL'])
-
-  scope :pending, -> { where(status: "pending") }
-  scope :active, -> { where(status: "active") }
-  scope :paid, -> { where(status: "paid") }
-  scope :active_or_paid, -> { where(status: ["active", "paid"]) }
-  scope :active_or_pending, -> { where(status: ["active", "pending"]) }
 end
