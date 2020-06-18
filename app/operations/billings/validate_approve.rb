42900b78dcc01792a51219488e66a5d90a379f0c
Raphael Alampay <raphael.alampay@gmail.com>
2020-06-18 10:37:36 +0800

    added validation of accounting entry for billing

--- app/operations/billings/validate_approve.rb
+++ app/operations/billings/validate_approve.rb
@@ -67,11 +67,47 @@ module Billings
         }
       end
 
+      validate_accounting_entry!
+
       #not_yet_implemented!
 
       @errors[:full_messages] = @errors[:messages].map{ |o| o[:message] }
 
       @errors
+    end
+
+    def validate_accounting_entry!
+      accounting_entry_data = @loan.data.with_indifferent_access[:accounting_entry]
+
+      dr_amount = 0.00
+      cr_amount = 0.00
+
+      if accounting_entry_data[:journal_entries].any?
+        accounting_entry_data[:journal_entries].each do |o|
+          if o[:post_type] == "DR"
+            dr_amount += o[:amount].to_f.round(2)
+          end
+
+          if o[:post_type] == "CR"
+            cr_amount += o[:amount].to_f.round(2)
+          end
+        end
+
+        dr_amount = dr_amount.round(2)
+        cr_amount = cr_amount.round(2)
+
+        if dr_amount != cr_amount
+          @errors[:messages] << {
+            key: "accounting_entry",
+            message: "Accounting entry not balanced. DR: #{dr_amount} CR: #{cr_amount}"
+          }
+        end
+      else
+        @errors[:messages] << {
+          key: "accounting_entry",
+          message: "No journal entries found for accounting entry"
+        }
+      end
     end
   end
 end
