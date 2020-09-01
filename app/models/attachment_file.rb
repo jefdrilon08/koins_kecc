4f0774b56851c1ceb0aa7422f4e0189a62f9e7d1
Raphael Alampay <raphael.alampay@gmail.com>
2020-09-01 15:05:08 +0800

    using aws s3 for file uploads in development

--- app/models/attachment_file.rb
+++ app/models/attachment_file.rb
@@ -1,8 +1,17 @@
 class AttachmentFile < ApplicationRecord
+  NAMES = [
+    "BLIPFORM", 
+    "BC", 
+    "MC", 
+    "ID", 
+    "COHABITATION", 
+    "OTHERFILE"
+  ]
+
   belongs_to :member
-  belongs_to :claim
+  belongs_to :claim, optional: true
 
   has_one_attached :file
 
-  validates :file_name, presence: true
+  validates :file_name, presence: true, inclusion: { in: NAMES }
 end
