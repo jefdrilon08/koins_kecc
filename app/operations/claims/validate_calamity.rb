70260e60a8b700713ecebcb1949d0b64785f1561
hannahgraceb <bulataohannahgrace@gmail.com>
2020-10-08 10:56:43 +0800

    delete the claims payment, account name and number in claim loa pdf and form

--- app/operations/claims/validate_calamity.rb
+++ app/operations/claims/validate_calamity.rb
@@ -50,17 +50,17 @@ module Claims
         @errors << "Name of beneficiary is required"
       end
 
-      if @claims_payment.blank?
-        @errors << "Claims Payment field is required"
-      end
+      # if @claims_payment.blank?
+      #   @errors << "Claims Payment field is required"
+      # end
 
-      if @account_name.blank?
-        @errors << "Account name field is required"
-      end
+      # if @account_name.blank?
+      #   @errors << "Account name field is required"
+      # end
 
-      if @account_number.blank?
-        @errors << "Account number field is required"
-      end
+      # if @account_number.blank?
+      #   @errors << "Account number field is required"
+      # end
       #validate_calamity_duplication!
       return  @errors
     end
--- app/operations/claims/validate_kalinga.rb
+++ app/operations/claims/validate_kalinga.rb
@@ -91,17 +91,17 @@ module Claims
         @errors << "Expired KALINGA!"
       end
 
-      if @claims_payment.blank?
-        @errors << "Claims Payment field is required"
-      end
+      # if @claims_payment.blank?
+      #   @errors << "Claims Payment field is required"
+      # end
 
-      if @account_name.blank?
-        @errors << "Account name field is required"
-      end
+      # if @account_name.blank?
+      #   @errors << "Account name field is required"
+      # end
 
-      if @account_number.blank?
-        @errors << "Account number field is required"
-      end
+      # if @account_number.blank?
+      #   @errors << "Account number field is required"
+      # end
 
       #validate_kalinga_duplication!
       return  @errors
