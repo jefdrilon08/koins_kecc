ceff1813b31d9cc23bbeb2424f9bcc7695f4c728
Raphael Alampay <raphael.alampay@gmail.com>
2021-02-01 18:07:43 +0800

    remove puma worker killer

--- config/puma.rb
+++ config/puma.rb
@@ -38,19 +38,19 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 plugin :tmp_restart
 
 before_fork do
-  require 'puma_worker_killer'
-
-  PumaWorkerKiller.config do |config|
-    config.ram           = 1024 # MB
-    config.frequency     = 10 # seconds
-    config.percent_usage = 0.98
-    config.rolling_restart_frequency = 12 * 3600 # 12 hours in seconds, or 12.hours if using Rails
-
-    # setting this to false will not log lines like:
-    # PumaWorkerKiller: Consuming 54.34765625 mb with master and 2 workers.
-    config.reaper_status_logs = true
-
-    config.pre_term = -> (worker) { puts "Worker #{worker.inspect} being killed" }
-  end
-  PumaWorkerKiller.start
+#  require 'puma_worker_killer'
+#
+#  PumaWorkerKiller.config do |config|
+#    config.ram           = 1024 # MB
+#    config.frequency     = 10 # seconds
+#    config.percent_usage = 0.98
+#    config.rolling_restart_frequency = 12 * 3600 # 12 hours in seconds, or 12.hours if using Rails
+#
+#    # setting this to false will not log lines like:
+#    # PumaWorkerKiller: Consuming 54.34765625 mb with master and 2 workers.
+#    config.reaper_status_logs = true
+#
+#    config.pre_term = -> (worker) { puts "Worker #{worker.inspect} being killed" }
+#  end
+#  PumaWorkerKiller.start
 end
