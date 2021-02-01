aa329e200de52d4f566235ed9037e986879c2275
Raphael Alampay <raphael.alampay@gmail.com>
2021-02-01 18:42:02 +0800

    comments for puma config

--- config/puma.rb
+++ config/puma.rb
@@ -46,8 +46,8 @@ before_fork do
     config.percent_usage = 0.98
     config.rolling_restart_frequency = 12 * 3600  12 hours in seconds, or 12.hours if using Rails
 
-     setting this to false will not log lines like:
-     PumaWorkerKiller: Consuming 54.34765625 mb with master and 2 workers.
+#     setting this to false will not log lines like:
+#     PumaWorkerKiller: Consuming 54.34765625 mb with master and 2 workers.
     config.reaper_status_logs = true
 
     config.pre_term = -> (worker) { puts "Worker {worker.inspect} being killed" }
