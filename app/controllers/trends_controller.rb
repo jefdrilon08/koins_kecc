abda91a0fc659ac837588ca71e37cdf922fe98e5
Raphael Alampay <raphael.alampay@gmail.com>
2021-02-23 21:25:51 +0800

    bug fix for doubling of par

---
+++ app/controllers/trends_controller.rb
@@ -0,0 +1,14 @@
+class TrendsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @subheader_items = [
+      { text: "Trends" }
+    ]
+
+    @payload = {
+      userId: current_user.id,
+      xKoinsAppAuthSecret: ENV['KOINS_APP_AUTH_SECRET']
+    }
+  end
+end
