676a5ff0f53ec49e75359d15c009eda9e14941f6
laureanoaljon <laureanoaljon0392@gmail.com>
2021-03-24 13:17:18 +0800

    hotfix

--- app/operations/utils/get_current_date.rb
+++ app/operations/utils/get_current_date.rb
@@ -2,23 +2,18 @@ module Utils
   class GetCurrentDate
     def initialize(config:)
       @config = config
-
       @branch = @config[:branch]
+
+      if Settings.try(:defaults).try(:default_branch).try(:id).present?
+        @branch = Branch.where(id: Settings.try(:defaults).try(:default_branch).try(:id)).first
+      end 
     end
 
     def execute!
-      if Settings.activate_microloans
-        if @branch.present? and @branch.current_date.present?
-          @branch.current_date.to_date
-        else
-          Date.today
-        end
+      if @branch.present? and @branch.current_date.present?
+        @branch.current_date.to_date
       else
-        if Settings.current_date.present?
-          Settings.current_date
-        else
-          Date.today
-        end   
+        Date.today
       end
 
 #      if @branch.present? and Settings.branch_config.present?
