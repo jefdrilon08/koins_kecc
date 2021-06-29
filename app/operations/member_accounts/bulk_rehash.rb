ca9075c4a11b94b636ecdc758571ab1679e5b430
kasaganaka <mis2kdci@gmail.com>
2021-06-29 21:59:36 +0800

    bulk_rehash

--- app/operations/member_accounts/bulk_rehash.rb
+++ app/operations/member_accounts/bulk_rehash.rb
@@ -98,7 +98,8 @@ module MemberAccounts
                   INNER JOIN
                     account_transactions ON account_transactions.subsidiary_id = member_accounts.id AND account_transactions.status = 'approved'
                   WHERE
-                    member_accounts.branch_id = '#{@branch.id}'
+                    member_accounts.branch_id = '#{@branch.id}' and
+                    member_accounts.account_subtype = 'Personal Savings Account'
                   ORDER BY
                     member_accounts.id, member_accounts.account_type, member_accounts.account_subtype, account_transactions.transacted_at ASC, account_transactions.updated_at ASC
                 EOS
