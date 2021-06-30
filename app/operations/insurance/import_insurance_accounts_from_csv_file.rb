871a9fec266d9b5c6d0576525653dde6367bca4c
laureanoaljon <laureanoaljon0392@gmail.com>
2021-06-30 15:33:08 +0800

    refactor code in uploading data from kcoop to kmba (partial)

--- app/operations/insurance/import_insurance_accounts_from_csv_file.rb
+++ app/operations/insurance/import_insurance_accounts_from_csv_file.rb
@@ -13,73 +13,56 @@ module Insurance
     def load_csv_file!
       CSV.foreach(@file.path, headers: true) do |row|
         member_id = row['member_id']
-        member_uuid = row['member_uuid']
-        member = Member.find(member_uuid)
-
-        if row['insurance_type'] == "LIF"
-          acc_subtype = "Life Insurance Fund"
-        elsif  row['insurance_type'] == "RF"
-          acc_subtype = "Retirement Fund"
-        elsif  row['insurance_type'] == "CLIP"
-          acc_subtype = "Credit Life Insurance Plan"
-        elsif  row['insurance_type'] == "HIIP"
-          acc_subtype = "Hospital Income Insurance Plan"
-        elsif  row['insurance_type'] == "PL"
-          acc_subtype = "Policy Loan"    
-        end
+        member = Member.find(member_id)
 
         if !member.nil?
           insurance_account = MemberAccount.where(id: row['uuid']).first
           
           if !insurance_account.nil?
-            if row['equity_value'].nil?
-              insurance_account.update!(
-                status: row['status'],
-                balance: row['balance'],
-                member_id: member_uuid,
-                branch: member.branch,
-                center: member.center
-              )
+            if row['data'].present?
+              ia_data = JSON.parse(row['data'])
             else
-              ia_data = insurance_account.data
-              
-              if !ia_data.nil?
-                ia_data[:equity_value] = row['equity_value']
-
-                insurance_account.update!(
-                  status: row['status'],
-                  balance: row['balance'],
-                  member_id: member_uuid,
-                  branch: member.branch,
-                  center: member.center,
-                  data: ia_data
-                )
-              else
-                insurance_account.update!(
-                  status: row['status'],
-                  balance: row['balance'],
-                  member_id: member_uuid,
-                  branch: member.branch,
-                  center: member.center,
-                  data: { equity_value: row['equity_value'] }
-                )
-              end
+              ia_data = nil
             end
+
+            insurance_account.update!(
+
+              member_id: row['member_id'],
+              account_type: row['account_type'],
+              account_subtype: row['account_subtype'],
+              balance: row['balance'],
+              center: row['center_id'],
+              branch: row['branch_id'],
+              status: row['status'],
+              maintaining_balance: row['maintaining_balance'],
+              created_at: row['created_at'],
+              updated_at: row['updated_at'],
+              data: ia_data
+            )
           else
             new_insurance_account = MemberAccount.new
+            
+            if row['data'].present?
+              ia_data = JSON.parse(row['data'])
+            else
+              ia_data = nil
+            end
 
             if !row['uuid'].nil?
               new_insurance_account.id = row['uuid']
             end
 
             new_insurance_account.member = member
-            new_insurance_account.balance = 0.00
-            new_insurance_account.account_type = "INSURANCE"
-            new_insurance_account.account_subtype = acc_subtype
-            new_insurance_account.status = row['status']
-            new_insurance_account.branch = member.branch
+            new_insurance_account.account_type = row['account_type']
+            new_insurance_account.account_subtype = row['account_subtype']
+            new_insurance_account.balance = row['balance']
             new_insurance_account.center = member.center
-            new_insurance_account.data = { equity_value: row['equity_value'] }
+            new_insurance_account.branch = member.branch
+            new_insurance_account.status = row['status']
+            new_insurance_account.maintaining_balance = row['maintaining_balance']
+            new_insurance_account.created_at = row['created_at']
+            new_insurance_account.updated_at = row['updated_at']
+            new_insurance_account.data = ia_data
 
             new_insurance_account.save!
           end
