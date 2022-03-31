78e6ebb05dc03c62b48965ae4a2b83b0bc87efc8
mis2kdci <mis2kdci@gmail.com>
2022-03-31 10:23:22 +0800

    hot fix

--- app/operations/loans/approve.rb
+++ app/operations/loans/approve.rb
@@ -115,7 +115,7 @@ module Loans
 
         if deduction_type == "share_capital_fee"
         
-          if @loan.cycle.to_i > 1 
+          if @member_data[:entry_point_loan_cycle].to_i > 1 
             if @loan.data["share_capital_available"].nil? || @loan.data["share_capital_available"] == false
               total_member_shares = MemberShare.where("member_id = ? and  certificate_for = ? and is_void is null",@member.id, "KCOOP").sum(:number_of_shares)
               @share_capital_deposit = Settings.defaults["share_capital_deposits"].last["regular_share_deposits"].select{ |a|   @loan.principal.to_f >= a["min_amount"]  and @loan.principal.to_f <= a["max_amount"]}
@@ -152,7 +152,7 @@ module Loans
                                                                   subsidiary_type: "MemberAccount",
                                                                   amount: @total_share_paid,
                                                                   transaction_type: "deposit",
-                                                                  transacted_at: @date_paid,
+                                                                  transacted_at: @current_date,
                                                                   status: "approved",
                                                                   data: acc_data
                                                                   )
