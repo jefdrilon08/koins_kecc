b02dd6063e9bd02e7baf3a549e28e92d6ff04f97
Raphael B Alampay <raphael.alampay@gmail.com>
2022-04-19 14:14:19 +0800

    use old defaults and config.load_defaults to 7.0

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module Koins
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.load_defaults 7.0
 
     # Configuration for the application, engines, and railties goes here.
     #
@@ -18,5 +18,20 @@ module Koins
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.time_zone = 'Asia/Manila'
+    config.active_record.default_timezone = :local # Or :utc
+
+    # Use sidekiq
+    config.active_job.queue_adapter = :sidekiq
+
+    config.generators do |g|
+      g.orm :active_record, primary_key_type: :uuid
+    end
+
+    # ActionCable
+    config.action_cable.mount_path = '/websocket'
+
+    #config.action_view.raise_on_missing_translations = true
   end
 end
