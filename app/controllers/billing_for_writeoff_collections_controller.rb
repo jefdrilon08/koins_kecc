f32bf4958d6a0de8cb9013d9f5919a499a99da9d
Ariel Agulay <iamyaelagulay@gmail.com>
2022-05-03 13:25:33 +0800

    Remove logic in view

--- app/controllers/billing_for_writeoff_collections_controller.rb
+++ app/controllers/billing_for_writeoff_collections_controller.rb
@@ -22,6 +22,10 @@ class BillingForWriteoffCollectionsController < DataStoreController
     @data_store = DataStore.find(params[:id])
     @member_with_writeoff = @data_store.data['record'].map{|o| "#{o['member_id']}"}
     @member_list = Member.where("id IN (?)" , @member_with_writeoff)
+    @member_list_item = @member_list.map{ |o| ["#{o["last_name"]}, #{o["first_name"]} ", o["id"] ] }
+
+    #raise @member_list_item.inspect
+    
     @subheader_items = [
       {
         text: "Billing for Writeoff Collections #{@member_list}"
