6ff0e1a6ad506a065eeeea027180fda68f34ceff
Ariel Agulay <iamyaelagulay@gmail.com>
2022-05-03 09:27:41 +0800

    add dropdown list of members for writeoff collection show

--- app/controllers/billing_for_writeoff_collections_controller.rb
+++ app/controllers/billing_for_writeoff_collections_controller.rb
@@ -19,8 +19,9 @@ class BillingForWriteoffCollectionsController < DataStoreController
   end
   
   def show
-    @billing_data_store = DataStore.find(params[:id])
-    @member_list = @billing_data_store.data['record'].map{|o| "#{o['member_id']}"}
+    @data_store = DataStore.find(params[:id])
+    @member_with_writeoff = @data_store.data['record'].map{|o| "#{o['member_id']}"}
+    @member_list = Member.where("id IN (?)" , @member_with_writeoff)
     @subheader_items = [
       {
         text: "Billing for Writeoff Collections #{@member_list}"
