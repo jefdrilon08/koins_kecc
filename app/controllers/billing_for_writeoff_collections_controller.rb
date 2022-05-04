d5348eb445810ab6a625150b048dccc19339f6fa
Ariel Agulay <iamyaelagulay@gmail.com>
2022-05-04 14:00:29 +0800

    remove enabled from dropdown list

--- app/controllers/billing_for_writeoff_collections_controller.rb
+++ app/controllers/billing_for_writeoff_collections_controller.rb
@@ -19,12 +19,14 @@ class BillingForWriteoffCollectionsController < DataStoreController
   end
   
   def show
-    @data_store           = DataStore.find(params[:id])
-    @member_with_writeoff = @data_store.data['record'].map{|o| "#{o['member_id']}"}
-    @member_list          = Member.where("id IN (?)" , @member_with_writeoff)
-    @member_list_item     = @member_list.map{ |o| ["#{o["last_name"]}, #{o["first_name"]} ", o["id"] ] }
-    @data                 = @data_store.data.with_indifferent_access
-    @data_view            = @data[:record].select{|x| x["enabled"] == true}
+    @data_store             = DataStore.find(params[:id])
+    #@member_with_writeoff = @data_store.data['record'].map{|o| "#{o['member_id']}"}
+    @get_members            = @data_store.data['record'].select{|o| o["enabled"] == false}
+    @member_with_writeoff  = @get_members.map{|o| "#{o['member_id']}"}
+    @member_list            = Member.where("id IN (?)" , @member_with_writeoff)
+    @member_list_item       = @member_list.map{ |o| ["#{o["last_name"]}, #{o["first_name"]} ", o["id"] ] }
+    @data                   = @data_store.data.with_indifferent_access
+    @data_view              = @data[:record].select{|x| x["enabled"] == true}
 
     @subheader_items = [
       {
