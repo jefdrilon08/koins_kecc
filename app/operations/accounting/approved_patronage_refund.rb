8fc0c9ceffad24057a68200563685dc79c23d38d
Raphael B Alampay <raphael.alampay@gmail.com>
2022-06-14 11:45:23 +0800

    removed (:db)

--- app/operations/accounting/approved_patronage_refund.rb
+++ app/operations/accounting/approved_patronage_refund.rb
@@ -89,8 +89,10 @@ module Accounting
         transaction_type  = 'deposit'
         transacted_at     = @date_approved
         status            = 'approved'
-        created_at        = Time.now.to_s(:db)
-        updated_at        = Time.now.to_s(:db)
+#        created_at        = Time.now.to_s(:db)
+#        updated_at        = Time.now.to_s(:db)
+        created_at        = Time.now.to_s
+        updated_at        = Time.now.to_s
 
         data  = {
           is_withdraw_payment: false,
@@ -120,8 +122,10 @@ module Accounting
         transaction_type  = 'deposit'
         transacted_at     = @date_approved
         status            = 'approved'
-        created_at        = Time.now.to_s(:db)
-        updated_at        = Time.now.to_s(:db)
+#        created_at        = Time.now.to_s(:db)
+#        updated_at        = Time.now.to_s(:db)
+        created_at        = Time.now.to_s
+        updated_at        = Time.now.to_s
 
         data  = {
           is_withdraw_payment: false,
