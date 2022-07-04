d85753e76ec9e7d7b5dfac45154b18008c1f10f1
mis2kdci <mis2kdci@gmail.com>
2022-07-04 09:41:38 +0800

    fix change value in deposit module

--- app/javascript/components/deposit_collections/DepositCollectionUITable.js
+++ app/javascript/components/deposit_collections/DepositCollectionUITable.js
@@ -3,6 +3,7 @@ import ReactTable from 'react-table';
 import Modal from 'react-modal';
 import Toggle from 'react-toggle';
 import "react-toggle/style.css";
+import $ from "jquery";
 
 import {numberWithCommas} from '../utils/helpers';
 import {customStyles} from '../utils/consts';
