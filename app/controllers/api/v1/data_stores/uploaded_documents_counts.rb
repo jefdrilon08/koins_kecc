6af397514edc840edae1053f81ac065a475f9884
mis2kdci <mis2kdci@gmail.com>
2023-02-17 08:24:13 +0800

    remove uploaded_documents_counts.rb

--- app/controllers/api/v1/data_stores/uploaded_documents_counts.rb
+++ app/controllers/api/v1/data_stores/uploaded_documents_counts.rb
@@ -5,42 +5,42 @@ module Api
         before_action :authenticate_user!
 
         def queue
-          @data_store_type  = "UPLOADED DOCUMENTS COUNTS"
-          @as_of            = params[:as_of].try(:to_date) || Date.today
-          @branch           = Branch.find(params[:branch_id])
+        #  @data_store_type  = "UPLOADED DOCUMENTS COUNTS"
+        #  @as_of            = params[:as_of].try(:to_date) || Date.today
+        #  @branch           = Branch.find(params[:branch_id])
 
-          @record = DataStore.uploaded_documents_counts.where(
-                      "meta->>'branch_id' = ? AND CAST(meta->>'as_of' AS date) = ?",
-                      @branch.id,
-                      @as_of
-                    ).first
+        #  @record = DataStore.uploaded_documents_counts.where(
+        #              "meta->>'branch_id' = ? AND CAST(meta->>'as_of' AS date) = ?",
+        #              @branch.id,
+        #              @as_of
+        #            ).first
 
-          if @record.blank?
-            @record = DataStore.create!(
-                        meta: {
-                          branch_id: @branch.id,
-                          branch_name: @branch.name,
-                          branch: {
-                            id: @branch.id,
-                            name: @branch.name
-                          },
-                          as_of: @as_of,
-                          data_store_type: @data_store_type
-                        },
-                        data: {
-                          status: "processing"
-                        }
-                      )
+        #  if @record.blank?
+        #    @record = DataStore.create!(
+        #                meta: {
+        #                  branch_id: @branch.id,
+        #                  branch_name: @branch.name,
+        #                  branch: {
+        #                    id: @branch.id,
+        #                    name: @branch.name
+        #                  },
+        #                  as_of: @as_of,
+        #                  data_store_type: @data_store_type
+        #                },
+        #                data: {
+        #                  status: "processing"
+        #                }
+        #              )
 
-            args  = {
-              record: @record,
-              data_store_type: @data_store_type
-            }
+        #    args  = {
+        #      record: @record,
+        #      data_store_type: @data_store_type
+        #    }
 
-            ProcessUploadedDocumentsCounts.perform_later(args)
+        #    ProcessUploadedDocumentsCounts.perform_later(args)
 
-            render json: { message: "ok" }
-          end
+        #    render json: { message: "ok" }
+        #  end
         end
       end
     end
