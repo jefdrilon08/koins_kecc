9dd42746c2a89c3ae41b26c862a7a3a72dce3166
Jomari <jomstaton@gmail.com>
2023-04-12 16:53:28 +0800

    API FOR KEZAR

--- app/operations/kmba/update_members.rb
+++ app/operations/kmba/update_members.rb
@@ -2,19 +2,51 @@ module Kmba
   class UpdateMembers
 
     def initialize(config:)
+      super()
       @config = config
-      @member_data = @config[:member_data]
-
-      member = Member.where(@member_data[:identification_number]).first
     end
 
     def execute!
+      @config.map{ |a|
+        member = Member.new(
+          center_id: a[:center_id],
+          branch_id: a[:branch_id],
+          first_name: a[:first_name],
+          middle_name: a[:middle_name],
+          last_name: a[:last_name],
+          gender: a[:gender],
+          date_of_birth: a[:date_of_birth],
+          civil_status: a[:civil_status],
+          home_number: a[:home_number],
+          mobile_number: a[:mobile_number],
+          processed_by: a[:processed_by],
+          approved_by: a[:approved_by],
+          identification_number: a[:identification_number],
+          place_of_birth: a[:place_of_birth],
+          status: a[:status],
+          member_type: a[:member_type],
+          religion: a[:religon],
+          insurance_status: a[:insustatus],  
+          data: a[:data],
+          access_token: a[:access_token],
+          signature_data: a[:signature_data],
+          modifiable: a[:modifiable],
+          previous_date_resigned: a[:previous_date_resigned],
+          insurance_date_resigned: a[:insurance_date_resigned],
+          member_id: a[:member_id],
+          encrypted_password: a[:encrypted_password],
+          username: a[:username],
+          online_application_id: a[:online_application_id],
+          membership_arrangement_id: a[:membership_arrangement_id],
+          membership_type_id: a[:membership_type_id],
+          referrer_id: a[:referrer_id],
+          coordinator_id: a[:coordinator_id],
+          email: a[:email]
+        )
 
-      member.update!(
-        members = @member_data
-      )
-
-       Rails.logger.info(puts " Member Identification Number : #{@config[:identification_number]} Record is up to date! ")
+        Rails.logger.info(puts " New Record is Save ID NO : #{a[:identification_number]}, saved! ")
+        member.save!
+      }
     end
   end
 end
--- app/operations/kmba/validate_save_members.rb
+++ app/operations/kmba/validate_save_members.rb
@@ -2,103 +2,113 @@ module Kmba
   class ValidateSaveMembers < AppValidator 
     def initialize(config:)
       super()
-
       @config               = config
+      # raise @config.inspect
     end
 
     def execute!
       #validate the member_data
 
-      raise @config.inspect
-      
-      # if @config["id"].blank?
-      #   @errors[:messages] << {
-      #     key: "id", 
-      #     message: "members not found"
-      #   }
-      # end
+      if @config.blank?
+        @errors[:messages] << {
+          key: "no_member", 
+          message: "No Member Record Found!"
+        }
+      else 
+        @config.map{ |a|
+          if a.blank?
+            @errors[:messages] << {
+              key: "no_member", 
+              message: "No Member Record Found!"
+            }
+          end
 
-      # if @member_data[:data][:address][:street].blank?
-      #   @errors[:messages] << {
-      #     key: "address_street",
-      #     message: "Address street required"
-      #   }
-      # end
+          if a[:first_name].blank?
+            @errors[:messages] << {
+              key: "first_name", 
+              message: "first_name not found"
+            }
+          end 
 
-      # if @member_data[:data][:address][:district].blank?
-      #   @errors[:messages] << {
-      #     key: "address_district",
-      #     message: "Address district required"
-      #   }
-      # end
-      #validate the first_name
-      # if @member_data[:first_name].blank?
-      #   @errors[:messages] << {
-      #     key: "first_name",
-      #     message: "First Name not Found"
-      #   }
-      # end 
+          if a[:first_name].nil?
+            @errors[:messages] << {
+              key: "first_name", 
+              message: "first_name not found"
+            }
+          end 
 
-      # #validate the middle_name
-      # # if @members_data[:middle_name].blank?  
-      # #   @errors << "Middle Name not Found"
-      # # end
-      
-      # if @member_data[:last_name].blank?
-      #   @errors[:messages] << {
-      #     key: "last_name",
-      #     message: "Last Name not Found"
-      #   }
-      # end
+          # Validate Middle Name
+          # if a[:middle_name].blank?
+          #   @errors[:messages] << {
+          #     key: "middle_name", 
+          #     message: "middle not found"
+          #   }
+          # end 
 
-      #  if @member_data[:identification_number].blank?
-      #   @errors[:messages] << {
-      #     key: "identification_number",
-      #     message: "Identification Number not Found"
-      #   }
-      # end 
+          if a[:last_name].blank?
+            @errors[:messages] << {
+              key: "middle_name", 
+              message: "middle not found"
+            }
+          end
 
-      # if @member_data[:gender].blank?
-      #   @errors[:messages] << {
-      #     key: "gender",
-      #     message: "Gender not Found"
-      #   }
-      # end
+          if a[:data][:address][:street].blank?
+            @errors[:messages] << {
+              key: "address_street", 
+              message: "Address Street not found"
+            }
+          end
 
-      # if @member_data[:date_of_birth].blank?
-      #   @errors[:messages] << {
-      #     key: "date_of_birth",
-      #     message: "Date of Birth is Empty"
-      #   }
-      # end
+          if a[:data][:address][:district].blank?
+            @errors[:messages] << {
+              key: "address_district",
+              message: "Address District not found"
+            }
+          end
 
-      # if @member_data[:civil_status].blank?
-      #   @errors[:messages] << {
-      #     key: "civil_status",
-      #     message: "Civil Status not Found"
-      #   }
-      # end
+          if a[:data][:address][:city].blank?
+            @errors[:messages] << {
+              key: "address_city",
+              message: "Address City not found"
+            }
+          end
 
-      # if @member_data[:member_type].blank?
-      #   @errors[:messages] << {
-      #     key: "member_type",
-      #     message: "Member Type not Found"
-      #   }
-      # end
+          if a[:date_of_birth].blank?
+            @errors[:messages] << {
+              key: "date_of_birth",
+              message: "Date of Birth not found"
+            }
+          end
 
-      # if @member_data[:data][:address][:street].blank?
-      #   @errors[:messages] << {
-      #     key: ":data:address:street",
-      #     message: "Address, street not Found"
-      #   }
-      # end
+          if a[:gender].blank?
+            @errors[:messages] << {
+              key: "gender",
+              message: "Gender not found"
+            }
+          end
 
-      # if @member_data[:data][:address][:district].blank?
-      #   @errors[:messages] << {
-      #     key: ":data:address:district",
-      #     message: "Address, district not Found"
-      #   }
-      # end
+          if a[:civil_status].blank?
+            @errors[:messages] << {
+              key: "civil_status",
+              message: "Civil Status not found"
+            }
+          end
+
+          if a[:branch_id].blank?
+            @errors[:messages] << {
+              key: "branch_id",
+              message: "Branch not Found"
+            }
+          end
+
+          if a[:center_id].blank?
+            @errors[:messages] << {
+              key: "center_id",
+              message: "Center not found"
+            }
+          end
+        }
+      end
 
       @errors[:messages].each do |o|
         @errors[:full_messages] << o[:message]
