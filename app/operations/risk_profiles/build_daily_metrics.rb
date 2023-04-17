850225ba14f4b560e629a33ead25882384b2f199
jef drilon <mis2kdci@gmail.com>
2023-04-17 14:25:12 +0800

    api risk profile

--- app/operations/risk_profiles/build_daily_metrics.rb
+++ app/operations/risk_profiles/build_daily_metrics.rb
@@ -9,7 +9,7 @@ module RiskProfiles
         
         @a = []
         DailyBranchMetric.where(as_of: @as_off).map{ |b| b}.each do |d|
-          sum_member = d[:data]["loaners"]["total"].to_i + d[:data]["pure_savers"]["total"] + d[:data]["active_members"]["total"]
+          sum_member = d[:data]["loaners"]["total"].to_i + d[:data]["pure_savers"]["total"] + d[:data]["active_members"]["total"] + d[:data]["inactive_member"]["total"]
           par_rate = d[:par] * 100
           @a << {
                 branch_id: d[:branch_id ],
