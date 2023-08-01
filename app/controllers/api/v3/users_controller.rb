2f0284450abc98000a76dbcb723384c9cfe0ee15
Raphael Alampay <raphael.alampay@gmail.com>
2023-08-01 14:41:37 +0800

    pass tests for update user v3

--- app/controllers/api/v3/users_controller.rb
+++ app/controllers/api/v3/users_controller.rb
@@ -3,6 +3,53 @@ module Api
     class UsersController < ::Api::V3::ApplicationController
       before_action :authenticate_user!
       before_action :authorize_mis!
+      before_action :load_resource!, only: [:update]
+
+      def load_resource!
+        @user = User.find_by_id(params[:id])
+
+        if @user.blank?
+          render json: { message: 'not found' }, status: :not_found
+        end
+      end
+
+      def update
+        validator = ::Core::Users::ValidateUpdate.new(
+          user:                   @user,
+          email:                  params[:email],
+          username:               params[:username],
+          identification_number:  params[:identification_number],
+          roles:                  params[:roles],
+          password:               params[:password],
+          password_confirmation:  params[:password_confirmation],
+          profile_picture:        params[:profile_picture]
+        )
+
+        validator.execute!
+
+        if validator.valid?
+          cmd = ::Core::Users::Update.new(
+            user:                   @user,
+            email:                  params[:email],
+            username:               params[:username],
+            identification_number:  params[:identification_number],
+            first_name:             params[:first_name],
+            last_name:              params[:last_name],
+            roles:                  params[:roles],
+            password:               params[:password],
+            password_confirmation:  params[:password_confirmation],
+            profile_picture:        params[:profile_picture],
+            is_regular:             params[:is_regular],
+            incentivized_date:      params[:incentivized_date]
+          )
+
+          cmd.execute!
+
+          render json: { id: cmd.user.id }
+        else
+          render json: validator.payload, status: :unprocessable_entity
+        end
+      end
 
       def create
         validator = ::Core::Users::ValidateCreate.new(
--- config/routes/api_v3.rb
+++ config/routes/api_v3.rb
@@ -1,5 +1,6 @@
 namespace :api do
   namespace :v3 do
     post "/users", to: "users#create"
+    put "/users/:id", to: "users#update"
   end
 end
