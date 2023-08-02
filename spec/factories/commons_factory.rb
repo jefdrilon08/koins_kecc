fee468d88ab0224b8485d37cd87d3990a0ce9aa6
Raphael Alampay <raphael.alampay@gmail.com>
2023-08-02 09:12:24 +0800

    added factories for area / cluster / branch / center

---
+++ spec/factories/commons_factory.rb
@@ -0,0 +1,26 @@
+FactoryBot.define do
+  factory :area do
+    name        { Faker::Name.last_name }
+    short_name  { Faker::Name.first_name }
+  end
+
+  factory :cluster do
+    area        { FactoryBot.create(:area) }
+    name        { Faker::Name.last_name }
+    short_name  { Faker::Name.first_name }
+  end
+
+  factory :branch do
+    cluster         { FactoryBot.create(:cluster) }
+    name            { Faker::Name.last_name }
+    short_name      { Faker::Name.first_name }
+    member_counter  { 0 }
+  end
+
+  factory :center do
+    branch      { FactoryBot.create(:branch) }
+    name        { Faker::Name.last_name }
+    short_name  { Faker::Name.first_name }
+    user        { FactoryBot.create(:user) }
+  end
+end
