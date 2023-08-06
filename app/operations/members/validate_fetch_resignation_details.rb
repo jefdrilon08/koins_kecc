bdd68751e49ff895a3d04d8b00012430c6549b17
geballeta <geballeta@gmail.com>
2023-08-06 22:17:47 +0800

    share capil resignation tapal

--- app/operations/members/validate_fetch_resignation_details.rb
+++ app/operations/members/validate_fetch_resignation_details.rb
@@ -7,6 +7,14 @@ module Members
 
       @member = @config[:member]
       @user   = @config[:user]
+      @resignation_settings     = Settings.resignation
+      @share_capital = MemberAccount.where(account_type: "EQUITY",account_subtype: "Share Capital",member_id: @member.id).first.balance.to_f
+      @cbu = MemberAccount.where(account_type: "EQUITY",account_subtype: "CBU",member_id: @member.id).first.balance.to_f
+      @mm_rec=MembershipPaymentRecord.where("member_id = ? and membership_name = ? and status = ? ", "#{@member.id}","K-KOOP","paid").last
+      @closing_fee                  = @resignation_settings.closing_fee
+      @number_of_years              = @resignation_settings.number_of_years
+      @total_equity_balance = 0.0
+
     end
 
     def execute!
@@ -19,14 +27,6 @@ module Members
         end
       end
 
-      if a.sum > 0
-        
-        @errors[:messages] << {
-          key: "member",
-          message: "Member have a total #{a.sum.round(2)} accrued interest to pay"
-          
-        }
-      end
       
       if @member.blank?
         @errors[:messages] << {
@@ -40,18 +40,110 @@ module Members
         }
       else
         active_loans  = Loan.active.where(member_id: @member.id)
-        
+        total_active_loans = active_loans.count
+        total_balance = 0.0
+        matured_loans_counts = 0
 
-        if active_loans.any?
+        #check years of membership
+        @closing_date = @mm_rec.date_paid + @number_of_years.years
+        if DateTime.now.to_date < @closing_date
+          @share_capital = @share_capital - 100.0
+        end
+
+        if active_loans.any?  
           active_loans.each do |o|
-            @errors[:messages] << {
-              key: "loan_#{o.id}",
-              message: "Active loan balance for #{o.loan_product.to_s}: #{o.total_balance}"
-            }
+            
+            #check if the loan is matured and count it 
+
+            if o.maturity_date.to_date < Date.today
+              matured_loans_counts += 1
+            end
+
+            #sum up all active loan balances
+            total_balance += o.total_balance.to_f
           end
         end
 
+        #add total balance of accrued
+        if a.any?
+          total_balance += a.sum
+        end
 
+        if @share_capital.to_f >= total_balance.to_f
+          @total_equity_balance = @share_capital.to_f
+        else
+          @total_equity_balance = (@share_capital + @cbu.to_f).round(2).to_f
+        end
+        #raise "if #{@total_equity_balance.round(2).to_f} > #{total_balance.to_f.inspect} ".inspect
+
+        #check if all active loans are matured and share capital balance is equal to all loan balances
+
+
+              
+
+        if a.any? and total_active_loans == 0
+          if @total_equity_balance.to_f < total_balance.to_f
+            @errors[:messages] << {
+              key: "member",
+              message: "Member have a total #{a.sum.round(2)} accrued interest to pay and Total Equity Balance is not enough"
+            }
+          end
+        elsif a.any? and total_active_loans > 0 
+          if total_active_loans > matured_loans_counts
+            active_loans.each do |o|
+              @errors[:messages] << {
+                key: "loan_#{o.id}",
+                message: "Active loan balance for #{o.loan_product.to_s}: #{o.total_balance} and Maturity Date: #{o.maturity_date.to_date.strftime("%B %d, %Y")}"
+              } 
+            end
+
+          elsif total_active_loans == matured_loans_counts and @total_equity_balance < total_balance
+             active_loans.each do |o|
+               @errors[:messages] << {
+                  key: "loan_#{o.id}",
+                  message: "All Active loans are Matured but the Total Equity Balance is not enough"
+                } 
+            end
+          end
+        elsif a.nil? and total_active_loans > 0
+          if total_active_loans > matured_loans_counts
+             active_loans.each do |o|
+              @errors[:messages] << {
+                key: "loan_#{o.id}",
+                message: "Active loan balance for #{o.loan_product.to_s}: #{o.total_balance} and Maturity Date: #{o.maturity_date.to_date.strftime("%B %d, %Y")}"
+              } 
+            end
+          elsif total_active_loans == matured_loans_counts and @total_equity_balance < total_balance
+             @errors[:messages] << {
+                key: "loan_#{o.id}",
+                message: "All Active loans are Matured but the Total Equity Balance is not enough"
+              } 
+          end
+        end
+
+     
+
+
+          
+
+
+
+
+
+
+
+
+        # if  total_active_loans.to_i > matured_loans_counts.to_i or @total_equity_balance.to_f < total_balance.to_f 
+
+        #   if a.any? and total_active_loans.to_i > matured_loans_counts.to_i 
+        #     @errors[:messages] << {
+        #       key: "member",
+        #       message: "Member have a total #{a.sum.round(2)} accrued interest to pay"
+        #     }
+        #   end
+          
+     
+        # end
 
 
       end
