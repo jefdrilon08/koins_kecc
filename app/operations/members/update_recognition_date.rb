06ec90ed04ad124bb935e9739aee0c9b786c2b5f
Jomari <jomstaton@gmail.com>
2023-10-18 16:34:46 +0800

    active status

--- app/operations/members/update_recognition_date.rb
+++ app/operations/members/update_recognition_date.rb
@@ -1,17 +1,17 @@
 module Members
   class UpdateRecognitionDate
-    def initialize(member:, recognition_date:)
+    def initialize(member:, recognition_date:, status:)
       @member              = member
       @recognition_date    = recognition_date
       @member_data         = @member.data.with_indifferent_access
+      @status              = status
 
-    
-      @member_data[:recognition_date]
     end
 
     def execute!
       @member_data[:recognition_date] = @recognition_date
       @member.update!(data: @member_data)
+      @member.update!(status: @status)
     end
   end
 end
