408e38a2598142f14a5ce58cbb3e3514ee50129f
jefdrilon <jeffrydrilon@yahoo.com>
2023-11-08 14:09:16 +0800

    hotfix

--- app/operations/risk_profiles/build_year_end_metrics.rb
+++ app/operations/risk_profiles/build_year_end_metrics.rb
@@ -2,7 +2,8 @@ module RiskProfiles
   class BuildYearEndMetrics
     def initialize(config:)
       @as_off = config[:test].to_date
-      @a = @as_off.year
+      @a_year = @as_off.year - 1
+      
       #raise @as_off.inspect
       
     end
@@ -12,7 +13,7 @@ module RiskProfiles
         DailyBranchMetric.where(
                                 "EXTRACT(Month from as_of) = ? and
                                  EXTRACT(Day from as_of) = ? and
-                                 EXTRACT(YEAR from as_of) = ?",12,31,@a 
+                                 EXTRACT(YEAR from as_of) = ?",12,31,@a_year
                                 ).map{ |b| b}.each do |d|
           sum_member = d[:data]["loaners"]["total"].to_i + d[:data]["pure_savers"]["total"] + d[:data]["active_members"]["total"] + d[:data]["inactive_members"]["total"]
           par_rate = d[:par] * 100
