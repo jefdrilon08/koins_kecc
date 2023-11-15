8c751f5982eacc3394a612c349dce0fdabc6851b
geballeta <geballeta@gmail.com>
2023-11-15 11:50:44 +0800

    print entry

--- app/controllers/print_controller.rb
+++ app/controllers/print_controller.rb
@@ -19,8 +19,8 @@ class PrintController < ApplicationController
     elsif type == "print_entry_involuntary"
       collection_involuntary = params[:id]
       data = ::Print::BuildPrintEntryInvoluntary.new(config: collection_involuntary).execute!
-      collection_involuntary =  data
-      render "print/collection_for_involuntary", layout: "print"
+      @data_store =  data
+      render "print/collection_for_involuntary_entry", layout: "print"
     elsif type == "share_capital_involuntary_letter_list"
       id = params[:id]
       data = ::Print::BuildShareCapitalInvoluntaryMasterList.new(config: id).execute!
