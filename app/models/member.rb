b5d390612cd8bd5e0c3c115b50f25f252775c1fe
KMBA-Kaiser <veliliakaizer12345@gmail.com>
2023-11-28 15:20:41 +0800

    add writeoffmembers to validation

--- app/models/member.rb
+++ app/models/member.rb
@@ -92,6 +92,7 @@ class Member < ApplicationRecord
   scope :pending, -> { where(status: "pending").order("last_name ASC") }
   scope :resigned, -> { where(status: "resigned").order("last_name ASC") }
   scope :active_and_resigned, -> { where(status: ["active", "resigned"]).order("last_name ASC") }
+  scope :active_and_resigned_and_writeoff, -> { where(status: ["active", "resigned", "writeoff"]).order("last_name ASC") }
   scope :active_and_resigned_and_pending, -> { where(status: ["active", "resigned", "pending"]).order("last_name ASC") }
   scope :returning, -> { where("status = ? AND previous_date_resigned IS NOT NULL", "active").order("last_name ASC") }
   scope :insurance_resigned, -> { where(insurance_status: "resigned").order("last_name ASC") }
