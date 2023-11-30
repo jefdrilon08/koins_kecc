6b16de4a0f0d543f5a1e31f83bb1b9beb9a5a43c
jefdrilon <jeffrydrilon@yahoo.com>
2023-11-30 09:48:20 +0800

    hotfix for diams

--- app/operations/risk_profiles/build_daily_metrics.rb
+++ app/operations/risk_profiles/build_daily_metrics.rb
@@ -1,24 +1,35 @@
 module RiskProfiles
   class BuildDailyMetrics
     def initialize(config:)
-      @as_off = config[:test]
+      @as_off = config[:test].to_date
       #raise @as_off.inspect
 
     end
     def execute!
+        @get_month = @as_off.strftime("%m").to_i
+        @get_year = @as_off.year
+
+      
         
         @a = []
-        DailyBranchMetric.where(as_of: @as_off).map{ |b| b}.each do |d|
-          sum_member = d[:data]["loaners"]["total"].to_i + d[:data]["pure_savers"]["total"] + d[:data]["active_members"]["total"] + d[:data]["inactive_members"]["total"]
-          par_rate = d[:par] * 100
-          @a << {
-                branch_id: d[:branch_id ],
+        Branch.all.each do |d|
+          dbm = DailyBranchMetric.where("extract(year from as_of) = ? and extract(month from as_of) = ? and branch_id = ?", @get_year, @get_month, d.id).order(:as_of)
+          if dbm.present?
+            dbm_data = dbm.last.data
+            #dbm.last.map{ |b| b}.each do |d|
+              sum_member = dbm_data["loaners"]["total"].to_i + dbm_data["pure_savers"]["total"] + dbm_data["active_members"]["total"] + dbm_data["inactive_members"]["total"]
+              
+              par_rate = dbm.last.par.to_f * 100
+              @a << {
+                branch_id: dbm.last.branch_id ,
                 total_mebers: sum_member,
-                portfolio: d[:portfolio],
-                par_rate: par_rate,
-                par_amount: d[:par_amount]
+                portfolio: dbm.last.portfolio,
+                par_rate: par_rate.to_f.round(2),
+                par_amount: dbm.last.par_amount
 
                }
+            #end
+          end
         end
         @a
 
