610511269661008747554572ec6339cad5a0a68b
KMBA-Kaiser <veliliakaizer12345@gmail.com>
2024-06-19 10:59:53 +0800

    Change ApplicationController to ApiController

--- app/controllers/api/v1/insurance_fund_transfer_collections_controller.rb
+++ app/controllers/api/v1/insurance_fund_transfer_collections_controller.rb
@@ -1,6 +1,6 @@
 module Api
   module V1
-    class InsuranceFundTransferCollectionsController < ApplicationController
+    class InsuranceFundTransferCollectionsController < ApiController
       before_action :authenticate_user!
 
       def fetch
