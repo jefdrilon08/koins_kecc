521ea5b0e037bc7c4bf4adf1790c3833d5c84547
KMBA-Kaiser <veliliakaizer12345@gmail.com>
2024-10-02 09:59:46 +0800

    address api

--- app/operations/administration/administration_address/import_admin_address_from_csv_file.rb
+++ app/operations/administration/administration_address/import_admin_address_from_csv_file.rb
@@ -15,31 +15,32 @@ module Administration
 
       def load_csv_file!
         CSV.foreach(@file.path, headers: true) do |row|
-        id = row['id']
+          id = row['id']
 
-        if !id.nil?
-          admin_address_record = AdminAddress.where(id: id).first
+          if !id.nil?
+            admin_address_record = AdminAddress.where(id: id).first
 
-          if admin_address_record.nil?
-            admin_address = AdminAddress.new
+            if admin_address_record.nil?
+              admin_address = AdminAddress.new
 
-            if id.present?
-              admin_address.id = uuid
+              if id.present?
+                admin_address.id = uuid
+              end
+
+              admin_address.id = row['id']
+              admin_address.region_name = row['region_name']
+              admin_address.province_name = row['province_name']
+              admin_address.is_active = row['is_active']
+              admin_address.save!
+            else
+             
+              admin_address_record.update!( 
+                                        id: row['id'],
+                                        region_name: row['region_name'],
+                                        province_name: row['province_name'],
+                                        is_active: row['is_active']
+                                      )
             end
-
-            admin_address.id = row['id']
-            admin_address.region_name = row['region_name']
-            admin_address.province_name = row['province_name']
-            admin_address.is_active = row['is_active']
-            admin_address.save!
-          else
-           
-            admin_address_record.update!( 
-                                      id: row['id'],
-                                      region_name: row['region_name'],
-                                      province_name: row['province_name'],
-                                      is_active: row['is_active']
-                                    )
           end
         end
       end
