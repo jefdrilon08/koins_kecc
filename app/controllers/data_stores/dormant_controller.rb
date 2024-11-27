a1da984ca7ab2bf415200f2370172f97dc750236
Jayerico <jhericolubrico073100@gmail.com>
2024-11-27 13:15:00 +0800

    dormant:fix

--- app/controllers/data_stores/dormant_controller.rb
+++ app/controllers/data_stores/dormant_controller.rb
@@ -216,8 +216,8 @@ module DataStores
         download_excel.serialize "#{Rails.root}/tmp/#{filename}"
         send_file "#{Rails.root}/tmp/#{filename}", filename: filename, type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
       end
-
-      
     end
   end
+end
+end
 
